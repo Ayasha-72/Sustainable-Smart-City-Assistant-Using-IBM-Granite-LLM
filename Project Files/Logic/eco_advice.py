e49d78e28bd61341e2e5fb9d8e85b11ede3829c1
Shaik Ayasha Fathima <shaikayesha7250@gmail.com>
2025-06-29 11:51:27 +0530

    Update eco_advice.py

--- Project Files/Logic/eco_advice.py
+++ Project Files/Logic/eco_advice.py
@@ -1,18 +1,26 @@
-from ibm_watsonx_ai.foundation_models import ModelInference
+import os
+from dotenv import load_dotenv
 from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai.foundation_models import ModelInference
 
-creds = Credentials(api_key="cjhxM8OWLyjtVflsSDuTDb4TycWNRYAl3xMkcHbuuFq4", url="https://us-south.ml.cloud.ibm.com")
+load_dotenv()
+
+creds = Credentials(
+    api_key=os.getenv("WATSONX_API_KEY"),
+    url=os.getenv("WATSONX_URL")
+)
 
 model = ModelInference(
     model_id="ibm/granite-3-8b-instruct",
     credentials=creds,
-    project_id="245d62b5-7b2f-477f-8bf1-ece30f3fa579"
+    project_id=os.getenv("WATSONX_PROJECT_ID")
 )
 
 def get_eco_advice(topic: str) -> str:
     prompt = f"Give one eco-friendly tip about {topic}."
-    res = model.generate_text(prompt=prompt, params={"max_new_tokens": 100})
-    return res
+    response = model.generate_text(prompt=prompt, params={"max_new_tokens": 100})
+    return response
+
 
 
 
